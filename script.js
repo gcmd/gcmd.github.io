215a80b93c640a1714b24917f04bcc63fbffdee3
steelneck <steelneck>
2007-11-09 22:49:04 +0000

    Major website update

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ function skift(lager){
 if (is==1){eval(dl + obj + disp + ja); is=2;}
 else {eval(dl + obj + disp + nej); is=1;}}
 
-function visa(lager){eval(dl + obj + disp + ja);}
+function visa(lager){
+eval(dl + obj + disp + ja);}
 
 function titta(lager){
 eval(dl + "('general')" + vi + bort);
-eval(dl + "('pres')" + vi + bort);
+eval(dl + "('form')" + vi + bort);
 eval(dl + "('layo')" + vi + bort);
+eval(dl + "('conf')" + vi + bort);
 eval(dl + "('filt')" + vi + bort);
 eval(dl + "('prog')" + vi + bort);
 eval(dl + "('devi')" + vi + bort);
